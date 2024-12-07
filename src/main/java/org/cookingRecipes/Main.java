e45bcfcc9c21b204062c1bd6697de7b0c15c5638
Ilias Syringas <ilias.syringas@gmail.com>
2024-12-08 01:15:18 +0200

    υπολογισμός συνολικής διάρκειας της συνταγής

--- src/main/java/org/cookingRecipes/Main.java
+++ src/main/java/org/cookingRecipes/Main.java
@@ -1,7 +1,26 @@
 package org.cookingRecipes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("pancakes.cook");
+        try {
+            FileReader reader = new FileReader(file);
+            int data = reader.read();
+            while(data != -1) {
+                System.out.print((char)data);
+                data = reader.read();
+            }
+            reader.close();
+        }catch (FileNotFoundException e){
+            System.err.println("Wrong file name!");
+        } catch (IOException e) {
+            System.err.printf("Error: %s\n", e.getMessage());
+        }
+        new Time(file);
     }
 }
--- src/main/java/org/cookingRecipes/Time.java
+++ src/main/java/org/cookingRecipes/Time.java
@@ -1,4 +1,28 @@
 package org.cookingRecipes;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Time {
+    ArrayList<Integer> time = new ArrayList<>();
+    public Time(File f){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            String line;
+            while((line = reader.readLine()) != null){
+                if(line.contains("~")){
+                    time.add(Integer.parseInt(line.substring((line.indexOf("{")+1), line.indexOf("%"))));
+                }
+            }
+            int sum = 0;
+            for (Integer i : time) {
+                sum += i;
+            }
+            System.out.printf("\nΣυνολική ώρα:\n\t%d minutes", sum);
+            reader.close();
+        }
+        catch(IOException e){
+            System.err.printf("Error: %s\n", e.getMessage());
+        }
+    }
 }
